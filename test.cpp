3ac0ba69ac4b70bd7589403277cbdec54e2e064c
pernilla <stahlbring@gmail.com>
2024-02-27 00:03:34 +0100

    uppdate

--- test.cpp
+++ test.cpp
@@ -1,130 +1,83 @@
 #include "queue.h"
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
-#include <iostream>
-#include <algorithm>
-#include <string>
-#include <sstream>
 
 
-using ::testing::_;
-using ::testing::Invoke;
-using ::testing::NiceMock;
-using ::testing::Return;
+using ::testing::_; // Import Google Mock's underscore symbol
+using ::testing::Invoke; // Import Google Mock's Invoke function
+using ::testing::NiceMock; // Import Google Mock's NiceMock class
+using ::testing::Return; // Import Google Mock's Return function
 
-class Memory : public IMemory
+// stuct based on IMemory
+struct Memory : public IMemory
 {
     std::vector<void *> vec;
 
-public:
     MOCK_METHOD(void *, malloc, (size_t size), (override));
     MOCK_METHOD(void, free, (void *ptr), (override));
 
-    size_t memory_blocks(void) { return vec.size(); }
-
+    // WHEN WE ALLOCATE WE PUSH
+    // when allocate is called,
     void *allocate(size_t size)
-    {
+    { // we allocate memeory
         void *ptr{std::malloc(size)};
-
+        // check if the ptr i 0, if not
         if (ptr != nullptr)
         {
+            // push( vec.push_back(ptr) to vector
             vec.push_back(ptr);
         }
-
         return ptr;
     }
-
+    // WHEN WE RELaSE WE "DELETE"
     void release(void *ptr)
     {
+        // if remove finds a value it removes it to the end of the vector
+        // erace has 3 arguments , begin end and the value we looking for.  Then call release= std::free(ptr);
         vec.erase(std::remove(vec.begin(), vec.end(), ptr), vec.end());
         std::free(ptr);
     }
 
-    ~Memory() { EXPECT_EQ(0, vec.size()); }
+    // in the distructor we check if the vector is empty or not
+    ~Memory() { EXPECT_EQ(0, vec.size());}
 };
 
-
-/*template <typename T>
-class QueueFixture : public ::testing::Test
+template <typename T>
+class circularQueueFixture: public ::testing::Test
 {
-    const std::tuple<
+  const std::tuple<
         std::vector<int>,
         std::vector<float>,
         std::vector<std::string>>
-        allValues{
-            {1, 2, 3, 4, 5},
-            {1.5f, 2.5f, 3.5f, 4.5f, 5.5f},
-            {"aaa", "bbb", "ccc", "ddd", "eee"}};
+        allTestValues{
+            {1,2,3,4},
+            {1.5f, 2.5f, 3.5f, 4.5f},
+            {"pink", "floyd","frank","zappa"}};
 
 protected:
-    const std::vector<T> values{std::get<std::vector<T>>(allValues)};
-    NiceMock<Memory> mock;
-    Queue<T> queue{mock, 5}; // Initialize queue with maximum size 5
+   const std::vector<T> values{std::get<std::vector<T>>(allTestValues)};// T Get the sample values for the current type
+    NiceMock<Memory> mock; // Create a nice mock object for memory management
+    circularQueue<T> queue{mock}; // Create a instance queue object using the mock memory manager
 
-    void SetUp(void) override
-    {
-        EXPECT_EQ(5, queue.size()); // Initial size of the queue
-    }
+        void SetUp(void) override 
+        { // Setup method executed before each test case
 
-    void TearDown(void) override {}
-};
+             EXPECT_EQ(0,queue.size()); // Assert that the queue is initially empty
 
-using TestTypes = ::testing::Types<int, float, std::string>;
-TYPED_TEST_SUITE(QueueFixture, TestTypes);
+              // Set up mock expectations for malloc and free methods
+             EXPECT_CALL(mock, malloc(_))
+                .WillRepeatedly(Invoke(&mock, &Memory::allocate)); //can be called nuber of times
+             EXPECT_CALL(mock, free(_))
+                .WillRepeatedly(Invoke(&mock, &Memory::release));
 
-TYPED_TEST(QueueFixture, mallocFails)
-{
-    EXPECT_CALL(this->mock, malloc(_))
-        .WillRepeatedly(Return(nullptr));
-    EXPECT_FALSE(this->queue.enqueue(this->values[0]));
-}
+        
+            // Enqueue sample values into the queue
+            for (size_t i = 1; i <= values.size(); i++)
+            {
+                EXPECT_TRUE(queue.enqueue(values[i - 1])); // Enqueue each sample value
+                EXPECT_EQ(i, queue.size()); // Assert that the size of the queue increases after enqueueing
 
-TYPED_TEST(QueueFixture, testClear)
-{
-    EXPECT_EQ(this->values.size(), this->queue.size());
-    this->queue.clear();
-    EXPECT_EQ(0, this->mock.memory_blocks());
-    EXPECT_EQ(0, this->queue.size());
-}
-
-TYPED_TEST(QueueFixture, testAverage)
-{
-    if constexpr (std::is_same<TypeParam, int>::value || std::is_same<TypeParam, float>::value)
-    {
-        double average{0};
-        for (size_t i = 0; i < this->values.size(); i++)
-        {
-            average += static_cast<double>(this->values[i]);
+            }
         }
-        average /= this->values.size();
-
-        EXPECT_FLOAT_EQ(average, this->queue.average());
-    }
-}
-
-TYPED_TEST(QueueFixture, testDequeue)
-{
-    TypeParam item;
-    size_t size{this->queue.size()};
-
-    for (size_t i = 1; i <= size; i++)
-    {
-        EXPECT_TRUE(this->queue.dequeue(item));
-        EXPECT_EQ(size - i, this->queue.size());
-        EXPECT_EQ(item, this->values[i - 1]);
-    }
-
-    EXPECT_FALSE(this->queue.dequeue(item));
-}
-
-TYPED_TEST(QueueFixture, testMovable)
-{
-    Queue<TypeParam> temp{std::move(this->queue)};
-    EXPECT_EQ(this->values.size(), temp.size());
-    EXPECT_EQ(0, this->queue.size());
-
-    this->queue = std::move(temp);
-    EXPECT_EQ(this->values.size(), this->queue.size());
-    EXPECT_EQ(0, temp.size());
-}*/
-
+        void TearDown(void) override {} // Tear down method executed after each test case
+};
